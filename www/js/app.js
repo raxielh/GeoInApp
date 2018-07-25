0271f8d713d2d323e684bf593996494d5bc15400
Rodrigo Garcia <rodrigogarciahoyos@gmail.com>
2018-07-24 22:48:43 -0500

    raxielh

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,50 @@ $(function()
  
   });
 
-
+  $(".menu_bar").on('swiperight',function (e,data){
+      $('nav').animate({
+        left: '0',
+        display:'block'
+      });
+      contador = 0;
+  });
  
 });
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+
+    var user = firebase.auth().currentUser;
+    var name, email, photoUrl, uid, emailVerified;
+
+    if (user != null) {
+      name = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      emailVerified = user.emailVerified;
+      uid = user.uid;
+      $('#m_correo').text(user.email);
+    }
+    
+  }else{
+    location.href ="entrar.html";
+  }
+});
+
+function salir(){
+  $.confirm({
+      title: 'Confirmar!',
+      content: 'Estas seguro de cerrar sesion!',
+      buttons: {
+          confirmar: function () {
+            firebase.auth().signOut().then(function(){
+                location.href ="entrar.html";
+            }).catch(function(error){
+
+            });
+          },
+          cancelar: function () {
+          }
+      }
+  });
+}
